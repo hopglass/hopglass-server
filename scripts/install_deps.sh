88ef8c0a4233d8e8207c7d3e93e94cf67ac7af4f
Milan Pässler <me@pbb.lc>
2019-01-20 20:42:49 +0100

    remove install scripts

--- scripts/install_deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ "$(whoami)" = "root" ];
-then
-  echo don\'t execute this script as root!
-fi
-cd $(dirname $0)/..
-npm install
--- scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ "$(whoami)" = "root" ];
-then
-  echo don\'t execute this script as root!
-fi
-
-cd $(dirname $0)/..
-node hopglass-server --config /etc/hopglass-server/config.$1.json
